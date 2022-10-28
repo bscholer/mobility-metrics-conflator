531924e36138327ca05277789e1e176c6b281a01
Ben Scholer <bscholer@hntb.com>
2022-10-28 17:02:37 -0400

    nearest neighbor working well!

--- interpolate.py
+++ interpolate.py
@@ -1,6 +1,8 @@
+import json
 import os.path
 import time
 
+import shapely
 from shapely.geometry import LineString, Point
 from tqdm import tqdm
 import numpy as np
@@ -12,12 +14,15 @@ tqdm.pandas()
 
 UTM_ZONE_EPSG_CODE = "32611"
 INTERPOLATION_DISTANCE = 5  # the distance between interpolated points in meters
+LINE_PAD_DISTANCE = 0.1 # how much to pad the line by to make sure we don't get duplicate points at intersections
+MIN_LINE_LENGTH = 1 # meters
 USE_CACHE = False
-USE_SUBSET = True
-xmin = -117.212
-ymin = 32.673
-xmax = -117.102
-ymax = 32.767
+USE_SUBSET = False
+WRITE_DEBUG_SHAPEFILES = False
+xmin = -117.186
+ymin = 32.692
+xmax = -117.129
+ymax = 32.741
 
 
 def print_full(x):
@@ -34,88 +39,110 @@ def print_full(x):
     pd.reset_option('display.max_colwidth')
 
 
-def split_line(line):
-    distances = np.arange(0, line.length, INTERPOLATION_DISTANCE)
-    points = [line.interpolate(distance) for distance in distances] + [line.coords[-1]]
+def split_line(row):
+    line = row['geometry']
+    # pad the line to avoid duplicate points
+    target_length = line.length - LINE_PAD_DISTANCE * 2
+    distances = np.arange(LINE_PAD_DISTANCE, target_length, INTERPOLATION_DISTANCE)
+    distances = np.append(distances, [target_length - LINE_PAD_DISTANCE])
+    if len(distances) < 2:
+        # print geojson of line
+        print(row.name)
+        print(line.length, target_length, distances)
+    points = [line.interpolate(distance) for distance in distances] # + [line.interpolate(target_length - LINE_PAD_DISTANCE)]
     return LineString(points)
 
 
 # Load the data, convert to UTM for shapely, then interpolate points
 def load_roads_from_file():
-    with open('data/ref.geojson') as f:
-        if os.path.exists('cache/ref.pickle'):
-            print('loading roads from pickle')
-            df = pd.read_pickle('cache/ref.pickle')
-        else:
-            start = time.process_time()
-            df = geopandas.read_file(f)
-            df = df.set_index('ROADSEGID')
-            df.to_pickle('cache/ref.pickle')
-            print('loaded geojson in {} seconds'.format(time.process_time() - start))
+    if os.path.exists('cache/final.pickle') and USE_CACHE:
+        return pd.read_pickle('cache/final.pickle')
 
-        if USE_SUBSET:
-            df = df.cx[xmin:xmax, ymin:ymax]
-            print('subset to {} roads'.format(len(df)))
-
-        total_points_count = df['geometry'].apply(lambda x: len(x.coords)).sum()
-        print('total points: {}'.format(total_points_count))
-
-        if os.path.exists('cache/ref_utm.pickle') and USE_CACHE:
-            print('loading utm roads from pickle')
-            df = pd.read_pickle('cache/ref_utm.pickle')
-        else:
-            start = time.process_time()
-            df = df.to_crs(UTM_ZONE_EPSG_CODE)
-            df.to_pickle('cache/ref_utm.pickle')
-            print('converted to UTM in {} seconds'.format(time.process_time() - start))
-
-        # make points every INTERPOLATION_DISTANCE meters evenly along the line
-        if os.path.exists('cache/ref_utm_split.pickle') and USE_CACHE:
-            print('loading split utm roads from pickle')
-            df = pd.read_pickle('cache/ref_utm_split.pickle')
-        else:
-            start = time.process_time()
-            df['geometry'] = df.progress_apply(lambda row: split_line(row['geometry']), axis=1)
-            df.to_pickle('cache/ref_utm_split.pickle')
-            print('interpolated points in {} seconds'.format(time.process_time() - start))
-
-        total_points_count = df['geometry'].apply(lambda x: len(x.coords)).sum()
-        print('total points: {}'.format(total_points_count))
-
-        # reproject to wgs84 for geojson
+    if os.path.exists('cache/ref.pickle'):
+        print('loading roads from pickle')
+        df = pd.read_pickle('cache/ref.pickle')
+    else:
+        start = time.process_time()
+        df = geopandas.read_file('data/ref/ref.shp')
         df = df.to_crs('EPSG:4326')
+        df = df.set_index('ROADSEGID')
+        df.to_pickle('cache/ref.pickle')
+        print('loaded geojson in {} seconds'.format(time.process_time() - start))
 
-        # write to shapefile
+    if USE_SUBSET:
+        df = df.cx[xmin:xmax, ymin:ymax]
+        print('subset to {} roads'.format(len(df)))
+
+    total_points_count = df['geometry'].apply(lambda x: len(x.coords)).sum()
+    print('total points: {}'.format(total_points_count))
+
+    if os.path.exists('cache/ref_utm.pickle') and USE_CACHE:
+        print('loading utm roads from pickle')
+        df = pd.read_pickle('cache/ref_utm.pickle')
+    else:
+        start = time.process_time()
+        df = df.to_crs(UTM_ZONE_EPSG_CODE)
+        df.to_pickle('cache/ref_utm.pickle')
+        print('converted to UTM in {} seconds'.format(time.process_time() - start))
+
+    # remove roads that are too short and show how many were removed
+    orig_len = df.shape[0]
+    df = df[df['geometry'].apply(lambda x: x.length) > MIN_LINE_LENGTH]
+    print('removed {} roads that were too short'.format(orig_len - df.shape[0]))
+
+    # make points every INTERPOLATION_DISTANCE meters evenly along the line
+    if os.path.exists('cache/ref_utm_split.pickle') and USE_CACHE:
+        print('loading split utm roads from pickle')
+        df = pd.read_pickle('cache/ref_utm_split.pickle')
+    else:
+        start = time.process_time()
+        df['geometry'] = df.progress_apply(lambda row: split_line(row), axis=1)
+        df.to_pickle('cache/ref_utm_split.pickle')
+        print('interpolated points in {} seconds'.format(time.process_time() - start))
+
+    total_points_count = df['geometry'].apply(lambda x: len(x.coords)).sum()
+    print('total points: {}'.format(total_points_count))
+
+    # reproject to wgs84 for geojson
+    df = df.to_crs('EPSG:4326')
+
+    # write to shapefile
+    if WRITE_DEBUG_SHAPEFILES:
         df.drop(columns=['POSTDATE', 'ADDSEGDT']).to_file('data/ref_utm_split/shp.shp')
 
-        print('loaded {} roads'.format(df.shape[0]))
-        points_dict = {}
-        for index, row in tqdm(df.iterrows(), total=len(df)):
-            for point in row['geometry'].coords:
-                if point not in points_dict:
-                    points_dict[point] = []
-                points_dict[point].append(index)
-        # get just points that have multiple ids
-        dup_points_dict = {k: v for k, v in points_dict.items() if len(v) > 1}
-        # remove coords from lines that are duplicates
-        for index, row in tqdm(df.iterrows(), total=len(df)):
-            if index == 35346:
-                print('here')
-
-            coords = row['geometry'].coords
-            coords = [c for c in coords if c not in dup_points_dict]
-            if len(coords) < 2:
-                print(index, coords, row['LENGTH'])
-            else:
-                df.at[index, 'geometry'] = LineString(coords)
-        # write to shapefile
+    points_dict = {}
+    for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+        # if len(points_dict) > 5569564:
+        #     print(index, row['geometry'], len(points_dict))
+        for point in row['geometry'].coords:
+            if point not in points_dict:
+                points_dict[point] = []
+            points_dict[point].append(index)
+    # get just points that have multiple ids
+    dup_points_dict = {k: v for k, v in points_dict.items() if len(v) > 1}
+    print('found {} duplicate points'.format(len(dup_points_dict)))
+    # remove coords from lines that are duplicates
+    # for index, row in tqdm(df.iterrows(), total=len(df), desc='removing duplicate points'):
+    #     orig_len = len(row['geometry'].coords)
+    #     coords = row['geometry'].coords
+    #     coords = [c for c in coords if c not in dup_points_dict]
+    #     print('removed {} points from road {}'.format(orig_len - len(coords), index))
+    #     if len(coords) < 2:
+    #         print(index, coords, row['LENGTH'])
+    #     else:
+    #         df.at[index, 'geometry'] = LineString(coords)
+    # write to shapefile
+    if WRITE_DEBUG_SHAPEFILES:
         df.drop(columns=['POSTDATE', 'ADDSEGDT']).to_file('data/ref_utm_split_no_dup/shp.shp')
 
 
-        # use the keys as geometries and make a multipoint geometry but make points with them
-        with open('data/dup_points.geojson', 'w') as f:
-            f.write(geopandas.GeoSeries(map(lambda p: Point(p), dup_points_dict.keys())).to_json())
-        # data = json.load(f)
-        # with open('cache/ref.pickle', 'wb') as f:
-        #     pickle.dump(data, f)
-        # return data
+    # use the keys as geometries and make a multipoint geometry but make points with them
+    # with open('data/dup_points.geojson', 'w') as f:
+    #     f.write(geopandas.GeoSeries(map(lambda p: Point(p), dup_points_dict.keys())).to_json())
+
+    df.to_pickle('cache/final.pickle')
+    return df
+    # data = json.load(f)
+    # with open('cache/ref.pickle', 'wb') as f:
+    #     pickle.dump(data, f)
+    # return data
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pickle
 import time
 
 import geopandas
+import numpy as np
+import pandas as pd
+import shapely.wkt
 from math import radians
 from shapely.geometry import Point
 
@@ -16,6 +19,7 @@ USE_CACHE = False
 
 EARTH_RADIUS = 6371008  # meters
 
+tqdm.pandas()
 
 def create_points_dict(roads_df):
     points_dict = {}
@@ -41,7 +45,7 @@ def create_points_dict(roads_df):
     # return points
 
 
-def create_ball_tree():
+def create_ball_tree(leaf_size=40):
     if os.path.exists('data/ball_tree.pickle') and os.path.exists('data/ball_tree_ids.pickle') and USE_CACHE:
         with open('data/ball_tree.pickle', 'rb') as f:
             tree = pickle.load(f)
@@ -52,9 +56,9 @@ def create_ball_tree():
         roads = load_roads_from_file()
         points_dict = create_points_dict(roads)
         points = list(points_dict.keys())
-        points_radians = [[radians(point[0]), radians(point[1])] for point in points]
+        points_radians = [[np.deg2rad(point[0]), np.deg2rad(point[1])] for point in points]
         ids = list(points_dict.values())
-        tree = BallTree([list(point) for point in points_radians], metric='haversine')
+        tree = BallTree([list(point) for point in points_radians], metric='haversine', leaf_size=leaf_size)
         with open('data/ball_tree.pickle', 'wb') as f:
             pickle.dump(tree, f)
         with open('data/ball_tree_ids.pickle', 'wb') as f:
@@ -63,6 +67,11 @@ def create_ball_tree():
 
 
 def load_mds_from_file():
+    df = pd.read_csv('data/mds-trips.csv')
+    # load geom column as wkt to geodataframe
+    df['geom'] = df['geom'].progress_apply(lambda wkt: shapely.wkt.loads(wkt))
+    return df
+
     with open('data/mds.json') as f:
         data = json.load(f)
         return data['data']['trips']
@@ -88,19 +97,31 @@ def generate_geojson_with_mds(mds):
 
 
 def main():
+    leaf_size = 40
     start = time.process_time()
     tree, ids = create_ball_tree()
     print('loaded roads in {} seconds'.format(time.process_time() - start))
 
-    mds = load_mds_from_file()
-    points = extract_points_from_mds(mds)
-    points_radians = [[radians(point[0]), radians(point[1])] for point in points]
+    mds_df = load_mds_from_file()
+    mds_df['geom_rad'] = mds_df['geom'].apply(lambda p: [np.deg2rad(p.x), np.deg2rad(p.y)])
+    # ids = mds_df['id'].tolist()
+    points = mds_df['geom_rad'].tolist()
+    # points_radians = [[radians(point[0]), radians(point[1])] for point in points]
     start = time.process_time()
-    distances, indices = tree.query(points_radians, k=1)
-    print('queried {} points in {} seconds'.format(len(points), time.process_time() - start))
-    distances = distances * EARTH_RADIUS
+    distances, indices = tree.query(points, k=1)
+    print('leaf size {}: {} seconds'.format(leaf_size, time.process_time() - start))
+    mds_df['idx'] = indices
+    mds_df['distance'] = distances
+    mds_df['distance_meters'] = mds_df['distance'].apply(lambda d: d * EARTH_RADIUS)
+    mds_df['x'] = mds_df['geom'].apply(lambda p: p.x)
+    mds_df['y'] = mds_df['geom'].apply(lambda p: p.y)
+    # match idx to the indicies in ids
+    mds_df['ROADSEGID'] = mds_df['idx'].apply(lambda idx: ids[idx][0])
 
-    idx_of_max_distance = distances.argmax()
+
+    mds_df.drop(columns=['distance', 'geom_rad', 'geom', 'idx']).to_csv('data/mds-trips-with-roadsegid.csv'.format(leaf_size), index=False)
+
+    idx_of_max_distance = mds_df['distance'].argmax()
     max_distance = distances[idx_of_max_distance]
     max_distance_id = ids[indices[idx_of_max_distance][0]]
     print(points[idx_of_max_distance], max_distance, max_distance_id)
